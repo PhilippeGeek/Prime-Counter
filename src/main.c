e0bee72fbbfe3d9949b2a6c452faf360526241bb
Gabriel Augendre <gabriel@augendre.info>
2015-09-23 22:00:32 +0200

    Use multiple files for functions and type definition

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 /*
-   Copyright (C) 2014 Gabriel Augendre <gabriel@augendre.info>
+   Copyright (C) 2015 Gabriel Augendre <gabriel@augendre.info>
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -23,94 +23,16 @@
 #include <sys/time.h>
 #include <fcntl.h>
 
-#define CHUNK_SIZE 10000
-
-// Data passed to thread
-struct threadData {
-    int start;
-    int stop;
-    sem_t* sem;
-};
+#include "headers/functions.h"
+#include "headers/types.h"
 
 /**
  * Global variables declaration.
  * They will be used by all threads.
  */
 int total_counter = 0;
-sem_t* sem_counter;
-sem_t* sem_threads;
-
-// Functions declaration
-void* getPrimeCount(void* arg);
-void emptyBuffer();
-int isPrime(const int NUMBER);
-int getNumber(const char* nom, int min, int max);
-
-/**
- * Empty buffer.
- * To be called after trying to get an int.
- */
-void emptyBuffer() {
-    while (getchar() != '\n') ;
-}
-
-/**
- * Returns 1 if the given number is prime, 0 if not.
- *
- * Loops through all numbers from 3 to the integer part of the square root of the number.
- */
-int isPrime(const int NUMBER) {
-    if (NUMBER <= 1) // Negatives, 0 and 1 are not prime.
-        return 0;
-
-    if (NUMBER == 2) // Two is the only even prime number.
-        return 1;
-
-    if (NUMBER % 2 == 0) // Others are not prime.
-        return 0;
-
-    int i;
-    int square = (int) sqrt(NUMBER);
-    /* We already checked 0, 1 and 2.
-     * + When checking if N = a * b, either a or b is lower or equal to sqrt(N).
-     * Checking above sqrt(N) would be useless.
-     */
-    for (i = 3; i <= square; ++i) {
-        if (NUMBER % i == 0)
-            return 0;
-    }
-    return 1;
-}
-
-/**
- * Get the number from the user.
- * Ensures it will return a positive integer.
- * Will loop until it gets a correct number from the user.
- *
- * 123foo is considered as 123.
- * foo123 is refused.
- * <space>123 is considered as 123.
- */
-int getNumber(const char* nom, int min, int max) {
-    int ok = 0;
-    int n = 0;
-    int checkMax = 1;
-    if (max <= min) {
-        checkMax = 0;
-    }
-    do {
-        if (checkMax) {
-            printf("Input %d < %s < %d : ", min - 1, nom, max + 1);
-        }
-        else {
-            printf("Input %s > %d : ", nom, min - 1);
-        }
-        ok = scanf("%d", &n);
-        emptyBuffer();
-    } while (!ok || n < min || (checkMax && n > max));
-
-    return n;
-}
+sem_t* sem_counter = NULL;
+sem_t* sem_threads = NULL;
 
 /**
  * Can receive two arguments (positive integer) :
@@ -171,7 +93,7 @@ int main(int argc, const char *argv[]) {
     }
 
     // ----- Reserving data for job queue
-    struct threadData* jobs = (struct threadData*) malloc(n * sizeof(struct threadData));
+    ThreadData* jobs = (ThreadData*) malloc(n * sizeof(ThreadData));
     if (jobs == NULL) {
         printf("Not enough memory available.\nBOOOOM :)\n");
         exit(42);
@@ -180,8 +102,8 @@ int main(int argc, const char *argv[]) {
     // ----- Putting data in job queue
     int i;
     int start = 0;
-    struct threadData* ptr;
-    struct threadData* end_ptr = jobs + n;
+    ThreadData* ptr;
+    ThreadData* end_ptr = jobs + n;
 
     // -- Counts how long is the number for allocating space for semaphore name
     int numberSize = 0;
@@ -257,32 +179,4 @@ int main(int argc, const char *argv[]) {
     printf("Took %lf seconds\n", elapsed);
 
     return 0;
-}
-
-/**
- * arg should be an instance of threadData struct.
- * Counts the number of primes between start and stop.
- * Increments the total_counter after the end of range.
- */
-void* getPrimeCount(void* arg) {
-    struct threadData* data = (struct threadData*) arg;
-    int start = data->start;
-    int stop = data->stop;
-    sem_post(data->sem);
-
-    int i;
-    int counter = 0;
-
-    for (i = start; i <= stop; ++i) {
-        if (isPrime(i)) {
-            ++counter;
-        }
-    }
-
-    sem_wait(sem_counter);
-    total_counter += counter;
-    sem_post(sem_counter);
-
-    sem_post(sem_threads);
-    pthread_exit(NULL);
 }
